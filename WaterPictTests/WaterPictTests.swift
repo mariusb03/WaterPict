39b405e3dbe46663fab62131ec1a6a7ffaaff8ba
mariusb03 <mariusbr6@gmail.com>
2025-01-08 15:33:20 +0100

    Initial Commit

---
+++ WaterPictTests/WaterPictTests.swift
@@ -0,0 +1,16 @@
+//
+//  WaterPictTests.swift
+//  WaterPictTests
+//
+//  Created by Marius Bringsvor Rusten on 08/01/2025.
+//
+
+import Testing
+
+struct WaterPictTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
